fd8cc989347bc1944342f58ae17f194af3576f32
nabinchowdhury <nabinchowdhury26@gmail.com>
2024-10-17 15:33:58 +0600

    cocepts added

---
+++ concept.py
@@ -0,0 +1,27 @@
+"""
+1. inheritance
+2. encapsulation
+3. polymorphism
+4. abstraction
+-----------------
+classes:
+1. User: base class (ABC)
+    2. Customer (User)
+    3. Employee (User)
+    4. Chef (Employee)
+    5. Server (Employee)
+    6. Manager(Employee)
+    7. cleaner (Employee)
+    8. supplier (User)
+    9. Marketer (User)
+
+10. FoodItem(ABC)
+    11. Pizza(FoodItem)
+    12. Burger(FoodItem)
+    13. Drinks(FoodItem)
+    14. Juice(FoodItem)
+    15. Salad(FoodItem)
+
+16. Order
+17. Restaurent
+"""
